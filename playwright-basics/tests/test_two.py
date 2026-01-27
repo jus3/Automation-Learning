9e5841dc2f78bbe135a5ad4e3183a57430ca14d8
jus3 <hetjodhani813@gmail.com>
2026-01-27 18:10:06 +0530

    27 Jan 2

--- playwright-basics/tests/test_two.py
+++ playwright-basics/tests/test_two.py
@@ -1,11 +1,11 @@
 from playwright.sync_api import Page, expect
 
 def test_open_playwright_site(page: Page):
-    page.goto("https://playwright.dev/")
-    expect(page).to_have_title("Fast and reliable end-to-end testing for modern web apps | Playwright")#Will check Title of the Page
+    page.goto("https://google.com")
+    expect(page).to_have_title("Google")#Will check Title of the Page
 
     # page.get_by_text("Get started").click()
-
-    page.get_by_role("button", name="Get started").click()
-    # page.get_by_role('link', name = 'Get started').click()
-    
+    page.get_by_title("Google")
+    page.get_by_role("combobox", name="Search").type("hello world")
+    page.get_by_label("Google Search").first.click()
+    # page.get_by_test_id("login-btnI").first.click()
